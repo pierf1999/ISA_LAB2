3e04a11ca8187a6b00987fcfdc697898cf5eff50
aledolme19 <s285624@studenti.polito.it>
2021-11-16 09:52:28 +0100

    Re-organzied

--- 4_version_mult_MBE_pipe_fineGrain/src/common_file/fpnormalize_fpnormalize.vhd
+++ 4_version_mult_MBE_pipe_fineGrain/src/common_file/fpnormalize_fpnormalize.vhd
@@ -13,37 +13,35 @@ USE ieee.std_logic_1164.all;
 USE ieee.std_logic_arith.all;
 USE ieee.std_logic_unsigned.all;
 
-
 ENTITY FPnormalize IS
-   GENERIC( 
-      SIG_width : integer := 28
-   );
-   PORT( 
-      SIG_in  : IN     std_logic_vector (SIG_width-1 DOWNTO 0);
-      EXP_in  : IN     std_logic_vector (7 DOWNTO 0);
-      SIG_out : OUT    std_logic_vector (SIG_width-1 DOWNTO 0);
-      EXP_out : OUT    std_logic_vector (7 DOWNTO 0)
-   );
+    GENERIC(
+        SIG_width : integer := 28
+    );
+    PORT(
+        SIG_in  : IN  std_logic_vector(SIG_width - 1 DOWNTO 0);
+        EXP_in  : IN  std_logic_vector(7 DOWNTO 0);
+        SIG_out : OUT std_logic_vector(SIG_width - 1 DOWNTO 0);
+        EXP_out : OUT std_logic_vector(7 DOWNTO 0)
+    );
 
--- Declarations
+    -- Declarations
 
-END FPnormalize ;
-
+END FPnormalize;
 
 -- hds interface_end
 ARCHITECTURE FPnormalize OF FPnormalize IS
 BEGIN
 
-PROCESS(SIG_in, EXP_in)
-BEGIN
-	IF (SIG_in( SIG_width-1 )='1') THEN
-		SIG_out <= '0' & SIG_in(SIG_width-1 DOWNTO 2) & (SIG_in(1) AND SIG_in(0));
-		EXP_out <= EXP_in + 1;
-	ELSE
-		SIG_out <= SIG_in;
-		EXP_out <= EXP_in;
-	END IF;
-END PROCESS;
+    PROCESS(SIG_in, EXP_in)
+    BEGIN
+        IF (SIG_in(SIG_width - 1) = '1') THEN
+            SIG_out <= '0' & SIG_in(SIG_width - 1 DOWNTO 2) & (SIG_in(1) AND SIG_in(0));
+            EXP_out <= EXP_in + 1;
+        ELSE
+            SIG_out <= SIG_in;
+            EXP_out <= EXP_in;
+        END IF;
+    END PROCESS;
 
 END FPnormalize;
 
